ebe7a44490b6a00ce5bdc1ad101bda4f4da83911
Lisa Taylor <ltaylor.lisa@gmail.com>
2024-07-31 19:26:09 -0700

    initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+  main
+    tier-one
+</template>
+
+<script lang="ts">
+  import { defineComponent, computed, ref, onMounted, watch } from "vue"
+  import TierOne from "@/components/TierOne.vue"
+
+  export default defineComponent({
+    props: {},
+    components: { TierOne },
+    setup(props, ctx) {
+      return {
+
+      }
+    }
+  })
+</script>
